22072d4e120a3cfc2dc58fd655400259d4a7eab7
LetsPrint3D <brett@fragnet.net>
2018-07-20 01:23:30 -0400

    Updated PROBE_OFFSET
    NPN sensor is mounted left of the extruder about 40mm and in front of it about 10mm, adjust as needed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -693,8 +693,8 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 10  // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER -40  // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -10  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
