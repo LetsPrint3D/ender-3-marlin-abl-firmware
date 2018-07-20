4378c4dd4b5a85aafe54d738348961bed1e08209
LetsPrint3D <brett@fragnet.net>
2018-07-19 21:41:13 -0400

    Updated X, Y and Z MAX_POS values
    Changed the X_MAX_POS, Y_MAX_POS and Z_MAX_POS to match the Ender-3 build volume values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -797,9 +797,9 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 20
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 400
+#define X_MAX_POS 235
+#define Y_MAX_POS 235
+#define Z_MAX_POS 260
 
 /**
  * Software Endstops
