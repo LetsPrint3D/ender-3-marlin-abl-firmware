42dac7c7473349abfd0b8f60152e23a1d960b780
LetsPrint3D <brett@fragnet.net>
2018-07-29 11:07:33 -0400

    Changed Z_BROBE_OFFSET
    Moved my ABL sensor to use a new mounting plate, adjusted the Z offset from extruder to reflect adjustment.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -695,7 +695,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -40  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -10  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER 2   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
